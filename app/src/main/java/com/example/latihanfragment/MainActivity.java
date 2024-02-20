9414648b17e30b9d92af01ee08d106179b5771d3
salman <salmanhafitalfarizy@gmail.com>
2024-02-20 14:48:02 +0700

    Initial commit

---
+++ app/src/main/java/com/example/latihanfragment/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.latihanfragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView tvText;
+    private BottomNavigationView navigation;
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        init();
+        navigationListener();
+    }
+
+    private void init() {
+        tvText = findViewById(R.id.tv_text);
+        navigation = findViewById(R.id.navigation);
+        webView = findViewById(R.id.webView);
+
+        WebSettings webSettings = webView.getSettings();  // Corrected variable name
+        webSettings.setJavaScriptEnabled(true);  // Corrected method name
+        webView.setWebViewClient(new WebViewClient());  // Corrected method name
+    }
+
+    private void navigationListener() {
+        navigation.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.nav_home) {
+                    tvText.setText(item.getTitle());
+                    webView.loadUrl("https://youtube.com");
+                    return true;
+                } else if (item.getItemId() == R.id.nav_search) {
+                    tvText.setText(item.getTitle());
+                    return true;
+                } else if (item.getItemId()
+                        == R.id.nav_notification) {
+                    tvText.setText(item.getTitle());
+                    return true;
+                } else if (item.getItemId() == R.id.nav_account) {
+                    tvText.setText(item.getTitle());
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+}
